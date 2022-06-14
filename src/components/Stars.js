44f5c4f35e96e578d1646506d54e305e4b25d04d
e.poluyan <e.poluyan@hsdigital.ru>
2022-06-14 20:32:17 +0700

    rating

---
+++ src/components/Stars.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import Star from './Star';
+
+function Stars({count = 0}) {
+  function getStar() {
+    const result = [];
+    for (let i = 0; i < count; i++) {
+      result.push(<li key={i}><Star/></li>)
+    }
+    return result;
+  }
+
+  if (typeof count === 'number' && count >= 1 && count <= 5) {
+    return (
+      <div className="content">
+        <ul className="stars">{getStar(count)}</ul>
+      </div>
+    );
+  }
+}
+
+Stars.propTypes = {
+  count: PropTypes.number.isRequired,
+}
+  
+export default Stars;
